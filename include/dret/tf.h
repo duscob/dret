1ebd2fd5027bc816d7a3173ede0b22d638011d65
Dustin Cobas <dustin.cobas@gmail.com>
2019-11-04 15:56:57 +0200

    Add functions to find first/last occurrence of a doc

--- include/dret/tf.h
+++ include/dret/tf.h
@@ -5,6 +5,11 @@
 #ifndef DRET_TF_H_
 #define DRET_TF_H_
 
+#include <cstddef>
+#include <utility>
+#include <cassert>
+
+
 namespace dret {
 
 template<typename _SLP, typename _OccBvs>
@@ -51,5 +56,86 @@ auto BuildFirstAndLastOccs(const _SLP &_slp) {
   return occs;
 }
 
+
+/**
+ * Find the relative position of the first occurrence for the document (_doc) in the expansion of the non-terminal (_var).
+ *
+ * The algorithm uses the bitvectors that mark the first and last occurrences of each document.
+ *
+ * @tparam _SLP Straight-line program
+ * @tparam _BVs Container of bitvectors
+ *
+ * @param _slp
+ * @param _var
+ * @param _doc
+ * @param _f_occs_bvs bitvectors marked with the first (leftmost) occurrences
+ * @param _l_occs_bvs bitvectors marked with the last (rightmost) occurrences
+ *
+ * @return relative position (>= 1) or 0 (if document doesn't appear)
+ */
+template<typename _SLP, typename _BVs>
+std::size_t FindFirstOcc(const _SLP &_slp, std::size_t _var, std::size_t _doc, const _BVs &_f_occs_bvs, const _BVs &_l_occs_bvs) {
+  assert(_doc <= _slp.Sigma());
+
+  if (_slp.IsTerminal(_var) && _var == _doc) {
+    return 1;
+  }
+
+  auto f_bit = _f_occs_bvs[_var][_doc];
+  auto l_bit = _l_occs_bvs[_var][_doc];
+
+  if (f_bit & ~l_bit) {
+    return 0;
+  }
+
+  auto children = _slp[_var];
+  if (!f_bit) {
+    return FindFirstOcc(_slp, children.first, _doc, _f_occs_bvs, _l_occs_bvs);
+  }
+  else {
+    return _slp.SpanLength(children.first) + FindFirstOcc(_slp, children.second, _doc, _f_occs_bvs, _l_occs_bvs);
+  }
+}
+
+
+/**
+ * Find the relative position of the last occurrence for the document (_doc) in the expansion of the non-terminal (_var).
+ *
+ * The algorithm uses the bitvectors that mark the first and last occurrences of each document.
+ *
+ * @tparam _SLP Straight-line program
+ * @tparam _BVs Container of bitvectors
+ *
+ * @param _slp
+ * @param _var
+ * @param _doc
+ * @param _f_occs_bvs bitvectors marked with the first (leftmost) occurrences
+ * @param _l_occs_bvs bitvectors marked with the last (rightmost) occurrences
+ *
+ * @return relative position (>= 1) or 0 (if document doesn't appear)
+ */
+template<typename _SLP, typename _BVs>
+std::size_t FindLastOcc(const _SLP &_slp, std::size_t _var, std::size_t _doc, const _BVs &_f_occs_bvs, const _BVs &_l_occs_bvs) {
+  assert(_doc <= _slp.Sigma());
+
+  if (_slp.IsTerminal(_var) && _var == _doc) {
+    return 1;
+  }
+
+  auto f_bit = _f_occs_bvs[_var][_doc];
+  auto l_bit = _l_occs_bvs[_var][_doc];
+
+  if (f_bit & ~l_bit) {
+    return 0;
+  }
+
+  auto children = _slp[_var];
+  if (l_bit) {
+    return _slp.SpanLength(children.first) + FindLastOcc(_slp, children.second, _doc, _f_occs_bvs, _l_occs_bvs);
+  }
+  else {
+    return FindLastOcc(_slp, children.first, _doc, _f_occs_bvs, _l_occs_bvs);
+  }
+}
 }
 #endif //DRET_TF_H_
--- test/tf_test.cpp
+++ test/tf_test.cpp
@@ -16,8 +16,11 @@
 
 using RightHand = std::pair<std::size_t, std::size_t>;
 using Rules = std::vector<RightHand>;
-using Occs = std::vector<sdsl::bit_vector>;
-using FLOccs = std::pair<Occs, Occs>;
+using OccsBVs = std::vector<sdsl::bit_vector>;
+using FLOccsBVs = std::pair<OccsBVs, OccsBVs>;
+using Occ = std::pair<std::pair<std::size_t, std::vector<std::size_t>>,
+                      std::vector<std::size_t>>; // (non-terminal, docs) = relative_positions
+using Occs = std::vector<Occ>;
 
 
 sdsl::bit_vector operator~(sdsl::bit_vector _bv) {
@@ -41,13 +44,14 @@ sdsl::bit_vector operator|(sdsl::bit_vector _bv1, const sdsl::bit_vector &_bv2) 
 }
 
 
-class FirstLastOccs_Test : public testing::TestWithParam<std::tuple<std::size_t, Rules, FLOccs>> {
+template<typename ... _Args>
+class TestWithSLP : public testing::TestWithParam<std::tuple<std::size_t, Rules, _Args...>> {
  protected:
   grammar::SLP<> slp_;
 
   void SetUp() override {
-    auto &sigma = std::get<0>(GetParam());
-    auto &rules = std::get<1>(GetParam());
+    auto &sigma = std::get<0>(this->GetParam());
+    auto &rules = std::get<1>(this->GetParam());
 
     slp_.Reset(sigma);
     for (auto &&rule : rules) {
@@ -57,14 +61,18 @@ class FirstLastOccs_Test : public testing::TestWithParam<std::tuple<std::size_t,
 };
 
 
+class FirstLastOccs_Test : public TestWithSLP<FLOccsBVs> {
+};
+
+
 TEST_P(FirstLastOccs_Test, Build) {
-  std::pair<Occs, Occs> occs = dret::BuildFirstAndLastOccs<sdsl::bit_vector>(slp_);
+  std::pair<OccsBVs, OccsBVs> occs_bvs = dret::BuildFirstAndLastOccs<sdsl::bit_vector>(slp_);
 
-  auto &e_f_occs = std::get<2>(GetParam()).first;
-  EXPECT_THAT(occs.first, testing::ElementsAreArray(e_f_occs));
+  auto &e_f_occs_bv = std::get<2>(GetParam()).first;
+  EXPECT_THAT(occs_bvs.first, testing::ElementsAreArray(e_f_occs_bv));
 
-  auto &e_l_occs = std::get<2>(GetParam()).second;
-  EXPECT_THAT(occs.second, testing::ElementsAreArray(e_l_occs));
+  auto &e_l_occs_bv = std::get<2>(GetParam()).second;
+  EXPECT_THAT(occs_bvs.second, testing::ElementsAreArray(e_l_occs_bv));
 }
 
 
@@ -73,22 +81,105 @@ INSTANTIATE_TEST_SUITE_P(FirstLastOccs,
                          testing::Values(
                              std::make_tuple(2ul,
                                              Rules{{1, 2}, {3, 3}},
-                                             FLOccs{
+                                             FLOccsBVs{
                                                  {sdsl::bit_vector(), {1, 0, 1}, {1, 1, 0}, {1, 0, 1}, {1, 0, 0}},
                                                  {sdsl::bit_vector(), {0, 1, 0}, {0, 0, 1}, {0, 0, 1}, {0, 1, 1}}}),
                              std::make_tuple(4ul,
                                              Rules{{2, 1}, {3, 5}, {3, 3}, {2, 5}, {4, 6}, {8, 1}, {6, 7}, {11, 6},
                                                    {9, 12}, {13, 10}},
-                                             FLOccs{
-                                                 {sdsl::bit_vector(), {1, 0, 1, 1, 1}, {1, 1, 0, 1, 1}, {1, 1, 1, 0, 1}, {1, 1, 1, 1, 0}, {1, 1, 0, 1, 1}, {1, 1, 1, 0, 1}, {1, 1, 1, 0, 1}, {1, 1, 0, 1, 1}, {1, 1, 1, 1, 0}, {1, 0, 0, 1, 1}, {1, 0, 0, 0, 1}, {1, 0, 0, 0, 1}, {1, 0, 0, 0, 0}, {1, 0, 0, 0, 0}},
-                                                 {sdsl::bit_vector(), {0, 1, 0, 0, 0}, {0, 0, 1, 0, 0}, {0, 0, 0, 1, 0}, {0, 0, 0, 0, 1}, {0, 1, 0, 0, 0}, {0, 1, 1, 0, 0}, {0, 0, 0, 1, 0}, {0, 1, 1, 0, 0}, {0, 1, 1, 1, 0}, {0, 1, 0, 0, 0}, {0, 0, 0, 1, 0}, {0, 1, 1, 1, 0}, {0, 1, 1, 1, 0}, {0, 1, 1, 0, 0}}})/*,
+                                             FLOccsBVs{
+                                                 {sdsl::bit_vector(), {1, 0, 1, 1, 1}, {1, 1, 0, 1, 1}, {1, 1, 1, 0, 1},
+                                                  {1, 1, 1, 1, 0}, {1, 1, 0, 1, 1}, {1, 1, 1, 0, 1}, {1, 1, 1, 0, 1},
+                                                  {1, 1, 0, 1, 1}, {1, 1, 1, 1, 0}, {1, 0, 0, 1, 1}, {1, 0, 0, 0, 1},
+                                                  {1, 0, 0, 0, 1}, {1, 0, 0, 0, 0}, {1, 0, 0, 0, 0}},
+                                                 {sdsl::bit_vector(), {0, 1, 0, 0, 0}, {0, 0, 1, 0, 0}, {0, 0, 0, 1, 0},
+                                                  {0, 0, 0, 0, 1}, {0, 1, 0, 0, 0}, {0, 1, 1, 0, 0}, {0, 0, 0, 1, 0},
+                                                  {0, 1, 1, 0, 0}, {0, 1, 1, 1, 0}, {0, 1, 0, 0, 0}, {0, 0, 0, 1, 0},
+                                                  {0, 1, 1, 1, 0}, {0, 1, 1, 1, 0}, {0, 1, 1, 0, 0}}})/*,
                              std::make_tuple(3ul,
                                              Rules{{2, 3}, {4, 1}, {5, 3}, {2, 1}, {1, 3}, {1, 4}, {7, 2}, {1, 5},
                                                    {9, 4}, {11, 8}, {12, 5}, {6, 10}, {15, 6},
                                                    {14, 16}, {17, 13}},
-                                             FLOccs{
+                                             FLOccsBVs{
                                                  {},
                                                  {}})*/
                          )
 );
 
+
+class FirstOccs_Test : public TestWithSLP<Occs> {};
+
+
+TEST_P(FirstOccs_Test, Find) {
+  auto[f_occs_bvs, l_occs_bvs] = dret::BuildFirstAndLastOccs<sdsl::bit_vector>(slp_);
+
+  auto occs = std::get<2>(GetParam());
+
+  for (const auto &occ : occs) {
+    auto &nonterminal = occ.first.first;
+    auto &docs = occ.first.second;
+    auto &e_poss = occ.second;
+
+    for (std::size_t i = 0; i < docs.size(); ++i) {
+      auto pos = dret::FindFirstOcc(slp_, nonterminal, docs[i], f_occs_bvs, l_occs_bvs);
+      EXPECT_EQ(pos, e_poss[i]) << nonterminal << ":" << docs[i];
+    }
+  }
+}
+
+
+INSTANTIATE_TEST_SUITE_P(FirstOccs,
+                         FirstOccs_Test,
+                         testing::Values(
+                             std::make_tuple(2ul,
+                                             Rules{{1, 2}, {3, 3}},
+                                             Occs{{{1, {1, 2}}, {1, 0}}, {{2, {2, 1}}, {1, 0}}, {{3, {1, 2}}, {1, 2}},
+                                                  {{4, {1, 2}}, {1, 2}}}),
+                             std::make_tuple(4ul,
+                                             Rules{{2, 1}, {3, 5}, {3, 3}, {2, 5}, {4, 6}, {8, 1}, {6, 7}, {11, 6},
+                                                   {9, 12}, {13, 10}},
+                                             Occs{{{1, {1, 2, 3, 4}}, {1, 0, 0, 0}}, {{2, {1, 2, 3, 4}}, {0, 1, 0, 0}},
+                                                  {{3, {1, 2, 3, 4}}, {0, 0, 1, 0}}, {{4, {1, 2, 3, 4}}, {0, 0, 0, 1}},
+                                                  {{5, {1, 2}}, {2, 1}}, {{6, {1, 2, 3}}, {3, 2, 1}},
+                                                  {{7, {1, 3}}, {0, 1}}, {{10, {1, 2}}, {3, 1}}})
+                         )
+);
+
+
+class LastOccs_Test : public TestWithSLP<Occs> {};
+
+
+TEST_P(LastOccs_Test, Find) {
+  auto[f_occs_bvs, l_occs_bvs] = dret::BuildFirstAndLastOccs<sdsl::bit_vector>(slp_);
+
+  auto occs = std::get<2>(GetParam());
+
+  for (const auto &occ : occs) {
+    auto &nonterminal = occ.first.first;
+    auto &docs = occ.first.second;
+    auto &e_poss = occ.second;
+
+    for (std::size_t i = 0; i < docs.size(); ++i) {
+      auto pos = dret::FindLastOcc(slp_, nonterminal, docs[i], f_occs_bvs, l_occs_bvs);
+      EXPECT_EQ(pos, e_poss[i]) << nonterminal << ":" << docs[i];
+    }
+  }
+}
+
+
+INSTANTIATE_TEST_SUITE_P(LastOccs,
+                         LastOccs_Test,
+                         testing::Values(
+                             std::make_tuple(2ul,
+                                             Rules{{1, 2}, {3, 3}},
+                                             Occs{{{1, {1, 2}}, {1, 0}}, {{2, {2, 1}}, {1, 0}}, {{3, {1, 2}}, {1, 2}},
+                                                  {{4, {1, 2}}, {3, 4}}}),
+                             std::make_tuple(4ul,
+                                             Rules{{2, 1}, {3, 5}, {3, 3}, {2, 5}, {4, 6}, {8, 1}, {6, 7}, {11, 6},
+                                                   {9, 12}, {13, 10}},
+                                             Occs{{{1, {1, 2, 3, 4}}, {1, 0, 0, 0}}, {{2, {1, 2, 3, 4}}, {0, 1, 0, 0}},
+                                                  {{3, {1, 2, 3, 4}}, {0, 0, 1, 0}}, {{4, {1, 2, 3, 4}}, {0, 0, 0, 1}},
+                                                  {{5, {1, 2}}, {2, 1}}, {{6, {1, 2, 3}}, {3, 2, 1}},
+                                                  {{7, {1, 3}}, {0, 2}}, {{10, {1, 2}}, {4, 2}}})
+                         )
+);
