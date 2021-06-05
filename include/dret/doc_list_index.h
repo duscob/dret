46d7a8f91b01dde01343b85e290c05269dfe86c2
Dustin Cobas <dustin.cobas@gmail.com>
2021-06-05 14:53:45 -0400

    Add basics interfaces for brute-force document listing

--- include/dret/doc_list_index.h
+++ include/dret/doc_list_index.h
@@ -19,23 +19,41 @@ class DocListIndex {
   virtual void Search(const TPattern &t_pattern, const std::function<void(TDocId)> &t_report) = 0;
 };
 
+template<typename TLocate, typename TGetDoc>
+class DocListIndexBrute : public DocListIndex {
+ public:
+  DocListIndexBrute(const TLocate &t_locate, const TGetDoc &t_get_doc) : locate_{t_locate}, get_doc_{t_get_doc} {}
+
+  void Search(const TPattern &t_pattern, const std::function<void(TDocId)> &t_report) override {
+    auto occurrences = locate_(t_pattern);
+
+    for (const auto &item : occurrences) {
+      t_report(get_doc_(item));
+    }
+  }
+
+ private:
+  TLocate locate_;
+  TGetDoc get_doc_;
+};
+
 template<typename TComputeSARange, typename TComputeDocs>
 class DocListIndexBasicScheme : public DocListIndex {
  public:
-  DocListIndexBasicScheme(const std::shared_ptr<TComputeSARange> &t_csa,
-                          const std::shared_ptr<TComputeDocs> &t_compute_docs)
+  DocListIndexBasicScheme(const TComputeSARange &t_csa,
+                          const TComputeDocs &t_compute_docs)
       : compute_sa_range_{t_csa}, compute_docs_{t_compute_docs} {
   }
 
   void Search(const TPattern &t_pattern, const std::function<void(TDocId)> &t_report) override {
-    auto[sp, ep] = (*compute_sa_range_)(t_pattern);
+    auto[sp, ep] = compute_sa_range_(t_pattern);
 
-    (*compute_docs_)(sp, ep, t_report);
+    compute_docs_(sp, ep, t_report);
   }
 
  private:
-  std::shared_ptr<TComputeSARange> compute_sa_range_;
-  std::shared_ptr<TComputeDocs> compute_docs_;
+  TComputeSARange compute_sa_range_;
+  TComputeDocs compute_docs_;
 };
 
 }
