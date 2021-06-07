712db4cf5f552df9d0d43ac2a7d983bb2a1f6778
Dustin Cobas <dustin.cobas@gmail.com>
2021-06-06 22:46:55 -0400

    Add benchmarks to brute-force document listing index

---
+++ benchmark/document_listing/bm_query_doc_list.cpp
@@ -0,0 +1,221 @@
+//
+// Created by Dustin Cobas <dustin.cobas@gmail.com> on 6/3/21.
+//
+
+#include <iostream>
+#include <functional>
+#include <bitset>
+
+#include <benchmark/benchmark.h>
+
+#include <gflags/gflags.h>
+
+#include <sdsl/config.hpp>
+
+#include "factory.h"
+
+DEFINE_string(patterns, "", "Patterns file. (MANDATORY)");
+DEFINE_string(data_dir, "./", "Data directory.");
+DEFINE_string(data_name, "data", "Data file basename.");
+DEFINE_bool(print_result, false, "Execute benchmark that print results per index.");
+
+static void SetupDefaultCounters(benchmark::State &t_state) {
+  t_state.counters["Size(bytes)"] = 0;
+  t_state.counters["Bits_x_Symbol"] = 0;
+  t_state.counters["Patterns"] = 0;
+  t_state.counters["Time_x_Pattern"] = 0;
+}
+
+// Benchmark Warm-up
+static void BM_WarmUp(benchmark::State &t_state) {
+  for (auto _ : t_state) {
+    std::vector<int> empty_vector(1000000, 0);
+  }
+
+  SetupDefaultCounters(t_state);
+}
+BENCHMARK(BM_WarmUp);
+
+class DocListResult {
+ public:
+  virtual ~DocListResult() = default;
+
+  virtual void operator()(std::size_t t_doc) = 0;
+  virtual void operator()() {}
+
+  virtual void Print(std::ostream &t_os) const = 0;
+};
+
+class DocListResultVector : public DocListResult {
+ public:
+  void operator()(std::size_t t_doc) override {
+    result_.emplace_back(t_doc);
+  }
+
+  void operator()() override {
+    sort(result_.begin(), result_.end());
+    result_.erase(unique(result_.begin(), result_.end()), result_.end());
+  }
+
+  void Print(std::ostream &t_os) const override {
+    for (const auto &item : result_) {
+      t_os << item << '\n';
+    }
+  }
+
+ private:
+  std::vector<std::size_t> result_;
+};
+
+class DocListResultBitvector : public DocListResult {
+ public:
+  explicit DocListResultBitvector(std::size_t t_size) : result_(t_size) {}
+
+  void operator()(std::size_t t_doc) override {
+    result_[t_doc] = true;
+  }
+
+  void Print(std::ostream &t_os) const override {
+    for (int i = 0; i < result_.size(); ++i) {
+      if (result_[i])
+        t_os << i << '\n';
+    }
+  }
+
+ private:
+  sdsl::bit_vector result_;
+};
+
+class DocListResultVectorBool : public DocListResult {
+ public:
+  explicit DocListResultVectorBool(std::size_t t_size) : result_(t_size) {}
+
+  void operator()(std::size_t t_doc) override {
+    result_[t_doc] = true;
+  }
+
+  void Print(std::ostream &t_os) const override {
+    for (int i = 0; i < result_.size(); ++i) {
+      if (result_[i])
+        t_os << i << '\n';
+    }
+  }
+
+ private:
+  std::vector<bool> result_;
+};
+
+// Benchmark Queries on Document Listing Index
+auto BM_QueryDocList = [](
+    benchmark::State &t_state, auto *t_factory, const auto &t_config, const auto &t_patterns, auto t_seq_size) {
+  auto[idx, idx_size] = t_factory->Make(t_config.config);
+
+  for (auto _ : t_state) {
+    for (const auto &pattern: t_patterns) {
+      auto result = t_config.create_result();
+      idx->Search(pattern, std::ref(*result));
+
+      (*result)();
+
+      delete result;
+    }
+  }
+
+  SetupDefaultCounters(t_state);
+  t_state.counters["Size(bytes)"] = idx_size;
+  t_state.counters["Bits_x_Symbol"] = idx_size * 8.0 / t_seq_size;
+  t_state.counters["Patterns"] = t_patterns.size();
+  t_state.counters["Time_x_Pattern"] = benchmark::Counter(
+      t_patterns.size(), benchmark::Counter::kIsIterationInvariantRate | benchmark::Counter::kInvert);
+};
+
+auto BM_PrintQueryDocList = [](
+    benchmark::State &t_state, const auto &t_idx_name, auto *t_factory, const auto &t_config, const auto &t_patterns) {
+  auto[idx, idx_size] = t_factory->Make(t_config.config);
+
+  for (auto _ : t_state) {
+    std::ofstream out(std::string("result-") + t_idx_name + ".txt");
+    for (const auto &pattern: t_patterns) {
+      out << pattern << std::endl;
+
+      auto result = t_config.create_result();
+      idx->Search(pattern, std::ref(*result));
+
+      (*result)();
+
+      result->Print(out);
+
+      delete result;
+    }
+  }
+
+  SetupDefaultCounters(t_state);
+};
+
+int main(int argc, char *argv[]) {
+  gflags::AllowCommandLineReparsing();
+  gflags::ParseCommandLineFlags(&argc, &argv, false);
+
+  if (FLAGS_patterns.empty() || FLAGS_data_name.empty() || FLAGS_data_dir.empty()) {
+    std::cerr << "Command-line error!!!" << std::endl;
+    return 1;
+  }
+
+  // Query patterns
+  std::vector<std::string> patterns;
+  {
+    std::ifstream pattern_file(FLAGS_patterns.c_str(), std::ios_base::binary);
+    if (!pattern_file) {
+      std::cerr << "ERROR: Failed to open patterns file!" << std::endl;
+      return 3;
+    }
+
+    std::string buf;
+    while (std::getline(pattern_file, buf)) {
+      if (buf.empty())
+        continue;
+
+      patterns.emplace_back(buf);
+    }
+    pattern_file.close();
+  }
+
+  // Benchmarks configs
+  sdsl::cache_config config(true, FLAGS_data_dir, FLAGS_data_name);
+
+  Factory factory(config);
+
+  struct Config {
+    const char *name;
+    Factory::Config config;
+    std::function<DocListResult *()> create_result;
+  };
+
+  auto create_result_vector = []() { return new DocListResultVector(); };
+  auto create_result_bitvector = [n_docs = factory.NDocs()]() { return new DocListResultBitvector(n_docs); };
+  auto create_result_vector_bool = [n_docs = factory.NDocs()]() { return new DocListResultVectorBool(n_docs); };
+
+  std::vector<Config> index_configs = {
+      {"Brute-R-Index-V", Factory::Config{Factory::IndexEnum::BRUTE}, create_result_vector},
+      {"Brute-R-Index-Bv", Factory::Config{Factory::IndexEnum::BRUTE}, create_result_bitvector},
+      {"Brute-R-Index-VB", Factory::Config{Factory::IndexEnum::BRUTE}, create_result_vector_bool},
+  };
+
+  auto seq_size = factory.SequenceSize();
+  std::string print_bm_prefix = "Print-";
+  for (const auto &idx_config : index_configs) {
+    benchmark::RegisterBenchmark(idx_config.name, BM_QueryDocList, &factory, idx_config, patterns, seq_size);
+
+    if (FLAGS_print_result) {
+      auto print_bm_name = print_bm_prefix + idx_config.name;
+      benchmark::RegisterBenchmark(
+          print_bm_name.c_str(), BM_PrintQueryDocList, idx_config.name, &factory, idx_config, patterns);
+    }
+  }
+
+  benchmark::Initialize(&argc, argv);
+  benchmark::RunSpecifiedBenchmarks();
+//  benchmark::Shutdown();
+
+  return 0;
+}
---
+++ benchmark/document_listing/factory.h
@@ -0,0 +1,126 @@
+//
+// Created by Dustin Cobas <dustin.cobas@gmail.com> on 6/3/21.
+//
+
+#ifndef DRET_BENCHMARK_DOCUMENT_LISTING_FACTORY_H_
+#define DRET_BENCHMARK_DOCUMENT_LISTING_FACTORY_H_
+
+#include <utility>
+
+#include <sdsl/config.hpp>
+#include <sdsl/io.hpp>
+
+#include <rindex/r_index.hpp>
+
+#include "../tool/definitions.h"
+
+#include "dret/doc_list_index.h"
+
+class Factory {
+ public:
+  enum class IndexEnum {
+    BRUTE
+  };
+
+  struct Config {
+    IndexEnum index_t;
+  };
+
+  explicit Factory(sdsl::cache_config t_config) : config_{std::move(t_config)} {
+    load(r_idx_);
+    size_r_idx_basic_ = r_idx_.item.size_in_bytes_basic();
+    seq_size_ = r_idx_.item.text_size();
+
+    load(doc_endings_);
+    load(doc_endings_rank_, [this]() { return TDocEndingsRank(&this->doc_endings_.item); });
+    n_doc_ = doc_endings_rank_.item(doc_endings_.item.size());
+  }
+
+  std::pair<dret::DocListIndex *, std::size_t> Make(const Config &t_config) {
+    return MakeInner(t_config);
+  }
+
+  [[nodiscard]] auto SequenceSize() const {
+    return seq_size_;
+  }
+
+  [[nodiscard]] auto NDocs() const {
+    return n_doc_;
+  }
+
+ private:
+
+  template<typename T>
+  struct Item {
+    std::string key;
+    bool initialized = false;
+    T item;
+    std::size_t size_in_bytes = 0;
+  };
+
+  template<typename T>
+  void load(Item<T> &t_item, const std::string &t_key) {
+    if (t_item.initialized) return;
+
+    if (!sdsl::cache_file_exists(t_key, config_))
+      std::cerr << "ERROR: File '" << sdsl::cache_file_name(t_key, config_) << "' not exist!!!";
+
+    sdsl::load_from_cache(t_item.item, t_key, config_);
+    t_item.initialized = true;
+    t_item.size_in_bytes = sdsl::size_in_bytes(t_item.item);
+  }
+
+  template<typename T>
+  void load(Item<T> &t_item) {
+    load(t_item, t_item.key);
+  }
+
+  template<typename T, typename TInit>
+  void load(Item<T> &t_item, const TInit &t_init) {
+    if (t_item.initialized) return;
+
+    t_item.item = t_init();
+    t_item.initialized = true;
+    t_item.size_in_bytes = sdsl::size_in_bytes(t_item.item);
+  }
+
+  std::pair<dret::DocListIndex *, std::size_t> MakeInner(const Config &t_config) {
+    dret::DocListIndex *index = nullptr;
+    std::size_t index_size = 0;
+
+    switch (t_config.index_t) {
+      case IndexEnum::BRUTE: {
+        auto locate = [this](const auto &tt_pattern) {
+          auto pattern = tt_pattern;
+          return this->r_idx_.item.locate_all(pattern);
+        };
+
+        index = new dret::DocListIndexBrute(locate, doc_endings_rank_.item);
+        index_size = r_idx_.size_in_bytes + doc_endings_rank_.size_in_bytes;
+        break;
+      }
+    }
+
+    return std::make_pair(index, index_size);
+  }
+
+  sdsl::cache_config config_;
+
+  std::size_t seq_size_;
+
+  // CSA
+  Item<ri::r_index<>> r_idx_ = {KEY_R_INDEX};
+  std::size_t size_r_idx_basic_ = 0;
+
+  // Document endings marks
+  using TDocEndings = sdsl::sd_vector<>;
+  using TDocEndingsRank = TDocEndings::rank_1_type;
+  Item<TDocEndings> doc_endings_ = {KEY_DOC_END};
+  Item<TDocEndingsRank> doc_endings_rank_;
+
+  // Documents
+  std::size_t n_doc_;
+
+};
+
+#endif //DRET_BENCHMARK_DOCUMENT_LISTING_FACTORY_H_
