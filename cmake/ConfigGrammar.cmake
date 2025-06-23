f7b0d5cada15f8699d308fc02e9c2771f99d9291
Dustin Cobas <dustin.cobas@gmail.com>
2025-06-22 21:14:06 -0400

    Update `ConfigGrammar.cmake` to use `compact` GIT_TAG and enable benchmarks by default

--- cmake/ConfigGrammar.cmake
+++ cmake/ConfigGrammar.cmake
@@ -12,13 +12,13 @@ endif ()
 include(DownloadProject)
 download_project(PROJ grammar
         GIT_REPOSITORY https://github.com/duscob/grammar.git
-        GIT_TAG master
+        GIT_TAG compact
         ${UPDATE_DISCONNECTED_IF_AVAILABLE})
 
 
 set(grammar_build_tools ON CACHE BOOL "grammar_build_tools")
 set(grammar_build_tests OFF CACHE BOOL "")
-set(grammar_build_benchmarks OFF CACHE BOOL "")
+set(grammar_build_benchmarks ON CACHE BOOL "")
 set(grammar_install OFF CACHE BOOL "grammar_install")
 
 add_subdirectory(${grammar_SOURCE_DIR} ${grammar_BINARY_DIR})
