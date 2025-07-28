0a08dccfd6a691b695993bf22cc4d7e64ea68958
Dustin Cobas <dustin.cobas@gmail.com>
2025-07-28 04:17:28 -0400

    Add Light SLP construction for Document Array in `doc_list_sampled_tree.h`
    - Introduce `ConstructLightSLPOnDA` function for Light SLP and Basic Light SLP construction.
    - Add configuration keys for Light SLP caching (`KEY_GCDA_LSLP`, `KEY_GCDA_LSLP_BASIC`).
    - Extend DA processing logic with Light SLP caching and computation tasks.

--- include/dret/doc_list_sampled_tree.h
+++ include/dret/doc_list_sampled_tree.h
@@ -23,6 +23,9 @@ namespace conf {
 const std::string KEY_GCDA_CSLP = "gcda_cslp";                // Combined SLP on Document Array
 const std::string KEY_GCDA_CSLP_DOCS = "gcda_cslp_docs";      // Chunks for documents
 const std::string KEY_GCDA_CSLP_DOCS_C = "gcda_cslp_docs_c";  //
+
+const std::string KEY_GCDA_LSLP = "gcda_lslp";
+const std::string KEY_GCDA_LSLP_BASIC = "gcda_lslp_basic";
 }  // namespace conf
 
 //~~~~~~~
@@ -61,6 +64,7 @@ class GCDA : public DLSampledTreeScheme<TStorage> {
 
 
 void ConstructCombinedSLPOnDA(Config& t_config, uint32_t t_block_size, float t_storing_factor);
+void ConstructLightSLPOnDA(Config& t_config);
 
 //~~~~~~~
 
@@ -90,11 +94,17 @@ void constructItems(GCDA<TStorage>& t_index, Config& t_config) {
   }
 
   // Construct Combined SLP of Document Array (Raw)
-  grammar::CombinedSLP<> cslp;
-  if (!sdsl::load_from_cache(cslp, conf::KEY_GCDA_CSLP, t_config)
+  if (!sdsl::cache_file_exists(conf::KEY_GCDA_CSLP, t_config)
       || !sdsl::cache_file_exists(conf::KEY_GCDA_CSLP_DOCS, t_config)) {
     auto event = sdsl::memory_monitor::event("DA-CombinedSLP");
     ConstructCombinedSLPOnDA(t_config, t_index.block_size(), t_index.storing_factor());
+  }
+
+  // Construct Light SLP of Document Array (Raw)
+  if (!sdsl::cache_file_exists(conf::KEY_GCDA_LSLP, t_config)
+      || !sdsl::cache_file_exists(conf::KEY_GCDA_LSLP_BASIC, t_config)) {
+    auto event = sdsl::memory_monitor::event("DA-LightSLP");
+    ConstructLightSLPOnDA(t_config);
   }
 }
 
@@ -125,6 +135,41 @@ inline void ConstructCombinedSLPOnDA(Config& t_config, uint32_t t_block_size, fl
   sdsl::store_to_cache(cslp_docs, conf::KEY_GCDA_CSLP_DOCS, t_config);
   grammar::Chunks<sdsl::int_vector<>, sdsl::int_vector<>> cslp_docs_c(cslp_docs, bit_compress, bit_compress);
   sdsl::store_to_cache(cslp_docs_c, conf::KEY_GCDA_CSLP_DOCS_C, t_config);
+}
+
+void ConstructLightSLPOnDA(Config& t_config) {
+  grammar::LightSLP<> lslp;
+
+  {
+    // Construct Light SLP on DA
+    auto datafile = cache_file_name(conf::KEY_DA_RAW, t_config);
+
+    grammar::SLP<> slp;
+    std::vector<std::size_t> compact_seq;
+    {
+      grammar::RePairReader<false> re_pair_reader;
+      auto slp_wrapper = grammar::BuildSLPWrapper(slp);
+
+      auto report_compact_seq = [&compact_seq](const auto& _var) { compact_seq.emplace_back(_var); };
+
+      re_pair_reader.Read(datafile, slp_wrapper, report_compact_seq);
+    }
+
+    grammar::CombinedSLP<> cslp;
+    sdsl::load_from_cache(cslp, conf::KEY_GCDA_CSLP, t_config);
+
+    lslp.Compute(slp, compact_seq, cslp);
+  }
+
+  sdsl::store_to_cache(lslp, conf::KEY_GCDA_LSLP, t_config);
+
+  // Construct Light SLP Basic on DA
+  auto bit_compress = [](sdsl::int_vector<>& _v) { sdsl::util::bit_compress(_v); };
+  grammar::LightSLP<grammar::BasicSLP<sdsl::int_vector<>>, grammar::SampledSLP<>,
+                    grammar::Chunks<sdsl::int_vector<>, sdsl::int_vector<>>>
+      lslp_basic(lslp, bit_compress, bit_compress, bit_compress, bit_compress);
+
+  sdsl::store_to_cache(lslp_basic, conf::KEY_GCDA_LSLP_BASIC, t_config);
 }
 
 //~~~~~~~
