15e9682ef59ab30c07254be380f97e6e77e9ebc5
Dustin Cobas <dustin.cobas@gmail.com>
2025-07-27 20:54:58 -0400

    Update Google Benchmark to v1.9.4

--- cmake/ConfigGBenchmark.cmake
+++ cmake/ConfigGBenchmark.cmake
@@ -3,7 +3,7 @@ include(FetchContent)
 FetchContent_Declare(
         googlebenchmark
         GIT_REPOSITORY https://github.com/google/benchmark.git
-        GIT_TAG "v1.9.0"
+        GIT_TAG v1.9.4
 )
 
 #Benchmark
