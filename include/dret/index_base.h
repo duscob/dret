555b1c65f21908fe6ae1a271e65c819a284f81ac
Dustin Cobas <dustin.cobas@gmail.com>
2025-12-30 15:40:01 -0500

    Introduce `IndexBaseWithExternalStorage` for enhanced loading and serialization logic
    - Add `IndexBaseWithExternalStorage` template class supporting external storage configurations.
    - Implement utility methods for item loading, serialization, and bit vector rank/select operations.
    - Simplify `GenericStorage` access with `get` and `set` templates.

--- include/dret/index_base.h
+++ include/dret/index_base.h
@@ -24,4 +24,165 @@ void construct(TIndex& t_index, const std::string& t_data_path, Config& t_config
   // t_index.load(t_config);
 }
 
+//~~~~~~~
+
+
+template <typename TItem>
+const TItem* get(const GenericStorage& t_storage, const std::string& t_key) {
+  auto it = t_storage.find(t_key);
+  return (it != t_storage.end()) ? std::any_cast<TItem>(&it->second) : nullptr;
+}
+
+template <typename TItem>
+const TItem* set(GenericStorage& t_storage, const std::string& t_key, TItem&& t_item) {
+  auto [it, inserted] = t_storage.emplace(t_key, t_item);
+  return std::any_cast<TItem>(&it->second);
+}
+
+//~~~~~~~
+
+
+template <typename TStorage = GenericStorage>
+class IndexBaseWithExternalStorage {
+ public:
+  typedef std::size_t size_type;
+
+  explicit IndexBaseWithExternalStorage(const TStorage& t_storage) : storage_{t_storage} {}
+
+  IndexBaseWithExternalStorage() = default;
+  virtual ~IndexBaseWithExternalStorage() = default;
+
+  auto sizeSequence() const {
+    return n_;
+  }
+
+  virtual void load(Config t_config) {
+    TSource source(std::ref(t_config));
+    loadInner(source, t_config.keys);
+  }
+
+  virtual void load(std::istream& in, const JSON& t_keys) {
+    TSource source(std::ref(in));
+    loadInner(source, t_keys);
+  }
+
+  virtual size_type serialize(std::ostream& out) const {
+    return serialize(out, nullptr, "");
+  }
+
+  virtual size_type serialize(std::ostream& out, sdsl::structure_tree_node* v, const std::string& name) const = 0;
+
+ protected:
+  using TSource = std::variant<std::reference_wrapper<Config>, std::reference_wrapper<std::istream>>;
+
+  virtual void loadInner(TSource& t_source, const JSON& t_keys) {}
+
+  template <typename TItem>
+  auto loadRawItem(const std::string& t_key, TSource& t_source, bool t_add_type_hash = false) {
+    auto item = get<TItem>(storage_, t_key);
+    if (!item) {
+      TItem data;
+      load(data, t_source, t_key, t_add_type_hash);
+
+      item = set(storage_, t_key, std::move(data));
+    }
+    return item;
+  }
+
+  template <typename TItem>
+  auto load(TItem& t_item, TSource t_source, const std::string& t_key, bool t_add_type_hash) {
+    std::visit(
+        [&t_item, &t_key, &t_add_type_hash, this](auto&& tt_source) {
+          return this->load(t_item, tt_source.get(), t_key, t_add_type_hash);
+        },
+        t_source);
+  }
+
+  template <typename TItem>
+  auto load(TItem& t_item, const sdsl::cache_config& t_config, const std::string& t_key, bool t_add_type_hash) {
+    if (!sdsl::load_from_cache(t_item, t_key, t_config, t_add_type_hash))
+      throw std::invalid_argument("File not found (Key: '" + t_key + "')");
+  }
+
+  template <typename TItem>
+  auto load(TItem& t_item, std::istream& t_in, const std::string& t_key, bool) {
+    sdsl::load(t_item, t_in);
+  }
+
+  template <typename TItem>
+  auto loadItem(const std::string& t_key, TSource& t_source, bool t_add_type_hash = false) {
+    auto item = loadRawItem<TItem>(t_key, t_source, t_add_type_hash);
+    return std::cref(*item);
+  }
+
+  template <typename TBv, typename TBvRank = typename TBv::rank_1_type>
+  auto loadBVRank(const std::string& t_key, TSource& t_source, bool t_add_type_hash = false) {
+    auto key_rank = t_key + "_rank";
+    auto item_rank = get<TBvRank>(storage_, key_rank);
+    if (!item_rank) {
+      auto item_bv = loadRawItem<TBv>(t_key, t_source, t_add_type_hash);
+
+      TBvRank rank;
+      load(rank, t_source, t_key, t_add_type_hash);
+      rank.set_vector(item_bv);
+
+      item_rank = set(storage_, key_rank, std::move(rank));
+    }
+
+    return std::cref(*item_rank);
+  }
+
+  template <typename TBv, typename TBvSelect = typename TBv::select_1_type>
+  auto loadBVSelect(const std::string& t_key, TSource& t_source, bool t_add_type_hash = false) {
+    auto key_select = t_key + "_select";
+    auto item_select = get<TBvSelect>(storage_, key_select);
+    if (!item_select) {
+      auto item_bv = loadRawItem<TBv>(t_key, t_source, t_add_type_hash);
+
+      TBvSelect select;
+      load(select, t_source, t_key, t_add_type_hash);
+      select.set_vector(item_bv);
+
+      item_select = set(storage_, key_select, std::move(select));
+    }
+
+    return std::cref(*item_select);
+  }
+
+  template <typename TItem>
+  std::size_t serializeItem(const std::string& t_key,
+                            std::ostream& out,
+                            sdsl::structure_tree_node* v,
+                            const std::string& name) const {
+    auto item = get<TItem>(storage_, t_key);
+    if (item) {
+      return sdsl::serialize(*item, out, v, name);
+    }
+    return sdsl::serialize_empty_object<TItem>(out, v, name);
+  }
+
+  template <typename TItem, typename TItemRank = typename TItem::rank_1_type>
+  std::size_t serializeRank(const std::string& t_key,
+                            std::ostream& out,
+                            sdsl::structure_tree_node* v,
+                            const std::string& name) const {
+    return serializeItem<TItemRank>(t_key + "_rank", out, v, name);
+  }
+
+  template <typename TItem, typename TItemSelect = typename TItem::select_1_type>
+  std::size_t serializeSelect(const std::string& t_key,
+                              std::ostream& out,
+                              sdsl::structure_tree_node* v,
+                              const std::string& name) const {
+    return serializeItem<TItemSelect>(t_key + "_select", out, v, name);
+  }
+
+  //********************
+  //********************
+  //********************
+
+  std::size_t n_ = 0;
+  TStorage storage_;
+};
+
 }  // namespace dret
