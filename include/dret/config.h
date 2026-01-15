9454a1d3cce8649bce0ca85b883fd00aead57986
Dustin Cobas <dustin.cobas@gmail.com>
2026-01-15 09:13:25 -0500

    Refactor `Config` to inherit from `sri::Config`, add `data_delim` member, and enhance configurability with constructor updates

--- include/dret/config.h
+++ include/dret/config.h
@@ -12,8 +12,6 @@ namespace dret {
 
 using sri::GenericStorage;
 
-using sri::Config;
-
 using sri::JSON;
 
 using sri::Alphabet;
@@ -21,8 +19,9 @@ using sri::Alphabet;
 //~~~~~~~
 
 namespace conf {
+using namespace sri::conf;
 constexpr std::string_view kDocEnds = "docEnd";
-}
+}  // namespace conf
 
 template <uint8_t t_width>
 auto createDefaultKeys() {
@@ -32,5 +31,21 @@ auto createDefaultKeys() {
 
   return keys;
 }
+
+struct Config : public sri::Config {
+  uint64_t data_delim = 0;
+
+  Config() = default;
+
+  Config(const std::filesystem::path& t_data_path,
+         const std::filesystem::path& t_output_dir,
+         sri::SAAlgo t_sa_algo,
+         bool t_delete_files = false,
+         uint8_t t_data_width = 8,
+         uint64_t t_data_delim = 0,
+         JSON t_keys = createDefaultKeys<DRET_DEFAULT_ALPHABET_WIDTH>())
+      : sri::Config(t_data_path, t_output_dir, t_sa_algo, t_delete_files, t_data_width, std::move(t_keys)),
+        data_delim(t_data_delim) {}
+};
 
 }  // namespace dret
