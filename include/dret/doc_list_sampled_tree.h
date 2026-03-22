bb980de2f2f7494d05659aefb75f40429800da58
Dustin Cobas <dustin.cobas@gmail.com>
2026-03-22 10:47:22 -0400

    Refactor `DLSampledTreeScheme` GCDA logic into a dedicated header file
    - Extract GCDA-related configuration, constructors, and utilities into `doc_list_sampled_tree_gcda.h`.
    - Refactor `constructItems` and `ConstructCombinedSLPOnDA` implementations for modularity.
    - Update `CMakeLists.txt` to include the new header.

--- include/dret/doc_list_sampled_tree.h
+++ include/dret/doc_list_sampled_tree.h
@@ -19,17 +19,6 @@
 
 namespace dret {
 
-namespace conf {
-const std::string KEY_GCDA_CSLP = "gcda_cslp";                // Combined SLP on Document Array
-const std::string KEY_GCDA_CSLP_DOCS = "gcda_cslp_docs";      // Chunks for documents
-const std::string KEY_GCDA_CSLP_DOCS_C = "gcda_cslp_docs_c";  //
-
-const std::string KEY_GCDA_LSLP = "gcda_lslp";
-const std::string KEY_GCDA_LSLP_BASIC = "gcda_lslp_basic";
-}  // namespace conf
-
-//~~~~~~~
-
 
 template <typename TStorage,
           typename TAlphabet,
@@ -118,157 +107,28 @@ class DLSampledTreeScheme : public DocListIndexExtStorage<TStorage, typename TAl
 //~~~~~~~
 
 
-template <typename TStorage = GenericStorage>
-class GCDA : public DLSampledTreeScheme<TStorage> {
- public:
-  using Base = DLSampledTreeScheme<TStorage>;
-  using typename Base::size_type;
-  using typename Base::TDocId;
-  using typename Base::TPattern;
-
-  GCDA(uint32_t t_block_size, float t_storing_factor) : block_size_(t_block_size), storing_factor_(t_storing_factor) {}
-
-  GCDA() = default;
-
-  size_type serialize(std::ostream& out, sdsl::structure_tree_node* v, const std::string& name) const override {
-    // TODO Add implementation
-    return 0;
+template <typename TStorage,
+          typename TAlphabet,
+          typename TCountIdx,
+          typename TComputeCover,
+          typename TGetDocs,
+          typename TGetDocSet,
+          typename TMergeSets>
+void construct(
+    DLSampledTreeScheme<TStorage, TAlphabet, TCountIdx, TComputeCover, TGetDocs, TGetDocSet, TMergeSets>& t_index,
+    Config& t_config) {
+  if (!cache_file_exists(t_config.keys[conf::kText].get<std::string>(), t_config)) {
+    auto event = sdsl::memory_monitor::event("Text");
+    ConstructText<TAlphabet::int_width>(t_config);
   }
 
-  const uint32_t& block_size() const {
-    return block_size_;
-  }
+  TCountIdx count_index(t_index.storage());
+  construct(count_index, t_config.data_path, t_config);
 
-  const float& storing_factor() const {
-    return storing_factor_;
-  }
-
- protected:
-  uint32_t block_size_ = 512;
-  float storing_factor_ = 4;
-};
-
-//~~~~~~~
-
-
-void ConstructCombinedSLPOnDA(Config& t_config, uint32_t t_block_size, float t_storing_factor);
-void ConstructLightSLPOnDA(Config& t_config);
-
-//~~~~~~~
-
-
-template <typename TStorage = GenericStorage>
-void constructItems(GCDA<TStorage>& t_index, Config& t_config) {
-  if (!sdsl::cache_file_exists(sdsl::conf::KEY_SA, t_config)) {
-    auto event = sdsl::memory_monitor::event("SA");
-    sdsl::construct_sa<8>(t_config);
-  }
-
-  if (!cache_file_exists(dret::conf::KEY_DOC_END, t_config)) {
-    auto event = sdsl::memory_monitor::event("DocEnds");
-    ConstructDocEnd(t_config);
-  }
-
-  if (!cache_file_exists(dret::conf::KEY_DA, t_config)) {
-    auto event = sdsl::memory_monitor::event("DA");
-    ConstructDocArray(t_config);
-  }
-
-  if (std::string file_da = cache_file_name(conf::KEY_DA_RAW, t_config);
-      !std::filesystem::exists(file_da + ".R") && REPAIR_EXE) {
-    auto event = sdsl::memory_monitor::event("DA-Repair");
-    std::string cmd = REPAIR_EXE + (" " + file_da);
-    std::system(cmd.c_str());
-  }
-
-  // Construct Combined SLP of Document Array (Raw)
-  if (!sdsl::cache_file_exists(conf::KEY_GCDA_CSLP, t_config)
-      || !sdsl::cache_file_exists(conf::KEY_GCDA_CSLP_DOCS, t_config)) {
-    auto event = sdsl::memory_monitor::event("DA-CombinedSLP");
-    ConstructCombinedSLPOnDA(t_config, t_index.block_size(), t_index.storing_factor());
-  }
-
-  // Construct Light SLP of Document Array (Raw)
-  if (!sdsl::cache_file_exists(conf::KEY_GCDA_LSLP, t_config)
-      || !sdsl::cache_file_exists(conf::KEY_GCDA_LSLP_BASIC, t_config)) {
-    auto event = sdsl::memory_monitor::event("DA-LightSLP");
-    ConstructLightSLPOnDA(t_config);
-  }
+  t_index.load(t_config);
 }
 
 //~~~~~~~
 
-
-inline void ConstructCombinedSLPOnDA(Config& t_config, uint32_t t_block_size, float t_storing_factor) {
-  auto datafile = cache_file_name(conf::KEY_DA_RAW, t_config);
-
-  grammar::SLP<> slp;
-  {
-    grammar::RePairReader<true> re_pair_reader;
-    auto slp_wrapper = grammar::BuildSLPWrapper(slp);
-    re_pair_reader.Read(datafile, slp_wrapper);
-  }
-
-  auto cslp = grammar::CombinedSLP<>(slp);
-  grammar::Chunks<> cslp_docs;
-
-  grammar::AddSet add_set(cslp_docs);
-  cslp.Compute(t_block_size,
-               add_set,
-               add_set,
-               grammar::MustBeSampled<decltype(cslp_docs)>(grammar::AreChildrenTooBig(cslp_docs, t_storing_factor)));
-
-  sdsl::store_to_cache(cslp, conf::KEY_GCDA_CSLP, t_config);
-
-  auto bit_compress = [](sdsl::int_vector<>& _v) {
-    sdsl::util::bit_compress(_v);
-  };
-
-  sdsl::store_to_cache(cslp_docs, conf::KEY_GCDA_CSLP_DOCS, t_config);
-  grammar::Chunks<sdsl::int_vector<>, sdsl::int_vector<>> cslp_docs_c(cslp_docs, bit_compress, bit_compress);
-  sdsl::store_to_cache(cslp_docs_c, conf::KEY_GCDA_CSLP_DOCS_C, t_config);
-}
-
-void ConstructLightSLPOnDA(Config& t_config) {
-  grammar::LightSLP<> lslp;
-
-  {
-    // Construct Light SLP on DA
-    auto datafile = cache_file_name(conf::KEY_DA_RAW, t_config);
-
-    grammar::SLP<> slp;
-    std::vector<std::size_t> compact_seq;
-    {
-      grammar::RePairReader<false> re_pair_reader;
-      auto slp_wrapper = grammar::BuildSLPWrapper(slp);
-
-      auto report_compact_seq = [&compact_seq](const auto& _var) {
-        compact_seq.emplace_back(_var);
-      };
-
-      re_pair_reader.Read(datafile, slp_wrapper, report_compact_seq);
-    }
-
-    grammar::CombinedSLP<> cslp;
-    sdsl::load_from_cache(cslp, conf::KEY_GCDA_CSLP, t_config);
-
-    lslp.Compute(slp, compact_seq, cslp);
-  }
-
-  sdsl::store_to_cache(lslp, conf::KEY_GCDA_LSLP, t_config);
-
-  // Construct Light SLP Basic on DA
-  auto bit_compress = [](sdsl::int_vector<>& _v) {
-    sdsl::util::bit_compress(_v);
-  };
-  grammar::LightSLP<grammar::BasicSLP<sdsl::int_vector<>>,
-                    grammar::SampledSLP<>,
-                    grammar::Chunks<sdsl::int_vector<>, sdsl::int_vector<>>>
-      lslp_basic(lslp, bit_compress, bit_compress, bit_compress, bit_compress);
-
-  sdsl::store_to_cache(lslp_basic, conf::KEY_GCDA_LSLP_BASIC, t_config);
-}
-
-//~~~~~~~
 
 }  // namespace dret
---
+++ include/dret/doc_list_sampled_tree_gcda.h
@@ -0,0 +1,202 @@
+//
+// Created by Dustin Cobas <dustin.cobas@gmail.com> on 3/21/26.
+//
+
+#pragma once
+
+#include "doc_list_sampled_tree.h"
+
+namespace dret {
+
+namespace conf {
+const std::string KEY_GCDA_CSLP = "gcda_cslp";                // Combined SLP on Document Array
+const std::string KEY_GCDA_CSLP_DOCS = "gcda_cslp_docs";      // Chunks for documents
+const std::string KEY_GCDA_CSLP_DOCS_C = "gcda_cslp_docs_c";  //
+
+const std::string KEY_GCDA_LSLP = "gcda_lslp";
+const std::string KEY_GCDA_LSLP_BASIC = "gcda_lslp_basic";
+}  // namespace conf
+
+//~~~~~~~
+
+
+template <typename TStorage,       // = GenericStorage,
+          typename TAlphabet,      // = Alphabet<>,
+          typename TCountIdx,      // = sri::SrIdxGeneric<sri::SrIndexValidArea<TStorage, TAlphabet>, 16>,
+          typename TComputeCover,  // = grammar::ComputeCoverBottomFunctor<grammar::LightSLP<>>,
+          typename TGetDocs,
+          typename TGetDocSet,
+          typename TMergeSets>
+class DocListIdxGCDA
+    : public DLSampledTreeScheme<TStorage, TAlphabet, TCountIdx, TComputeCover, TGetDocs, TGetDocSet, TMergeSets> {
+ public:
+  using Base = DLSampledTreeScheme<TStorage, TAlphabet, TCountIdx, TComputeCover, TGetDocs, TGetDocSet, TMergeSets>;
+  using typename Base::size_type;
+  using typename Base::TDocId;
+  using typename Base::TPattern;
+
+  explicit DocListIdxGCDA(const TStorage& t_storage) : Base(t_storage), count_idx_(t_storage) {}
+
+  DocListIdxGCDA(uint32_t t_block_size, float t_storing_factor)
+      : block_size_(t_block_size), storing_factor_(t_storing_factor) {}
+
+  DocListIdxGCDA() = default;
+
+  // size_type serialize(std::ostream& out, sdsl::structure_tree_node* v, const std::string& name) const override {
+  //   // TODO Add implementation
+  //   return 0;
+  // }
+
+  const uint32_t& block_size() const {
+    return block_size_;
+  }
+
+  const float& storing_factor() const {
+    return storing_factor_;
+  }
+
+ protected:
+  uint32_t block_size_ = 512;
+  float storing_factor_ = 4;
+
+  TCountIdx count_idx_;
+};
+
+//~~~~~~~
+
+
+void ConstructCombinedSLPOnDA(Config& t_config, uint32_t t_block_size, float t_storing_factor);
+void ConstructLightSLPOnDA(Config& t_config);
+
+//~~~~~~~
+
+
+template <typename TStorage = GenericStorage,
+          typename TAlphabet,
+          typename TCountIdx,
+          typename TComputeCover,
+          typename TGetDocs,
+          typename TGetDocSet,
+          typename TMergeSets>
+void constructItems(
+    DocListIdxGCDA<TStorage, TAlphabet, TCountIdx, TComputeCover, TGetDocs, TGetDocSet, TMergeSets>& t_index,
+    Config& t_config) {
+  if (!cache_file_exists(t_config.keys[conf::kText].get<std::string>(), t_config)) {
+    auto event = sdsl::memory_monitor::event("Text");
+    ConstructText<TAlphabet::int_width>(t_config);
+  }
+
+  if (const auto key = t_config.keys[conf::kSA].get<std::string>(); !cache_file_exists(key, t_config)) {
+    auto event = sdsl::memory_monitor::event(key);
+    sdsl::construct_sa<8>(t_config);
+  }
+
+  if (const auto key = t_config.keys[conf::kDocEnds].get<std::string>(); !cache_file_exists(key, t_config)) {
+    auto event = sdsl::memory_monitor::event(key);
+    ConstructDocEnd(t_config);
+  }
+
+  if (const auto key = t_config.keys[conf::kDA].get<std::string>(); !cache_file_exists(key, t_config)) {
+    auto event = sdsl::memory_monitor::event(key);
+    ConstructDocArray(t_config);
+  }
+
+  if (std::string file_da = cache_file_name(conf::KEY_DA_RAW, t_config);
+      !std::filesystem::exists(file_da + ".R") && REPAIR_EXE) {
+    auto event = sdsl::memory_monitor::event("DA-Repair");
+    std::string cmd = REPAIR_EXE + (" " + file_da);
+    std::system(cmd.c_str());
+  }
+
+  // Construct Combined SLP of Document Array (Raw)
+  if (!sdsl::cache_file_exists(conf::KEY_GCDA_CSLP, t_config)
+      || !sdsl::cache_file_exists(conf::KEY_GCDA_CSLP_DOCS, t_config)) {
+    auto event = sdsl::memory_monitor::event("DA-CombinedSLP");
+    ConstructCombinedSLPOnDA(t_config, t_index.block_size(), t_index.storing_factor());
+  }
+
+  // Construct Light SLP of Document Array (Raw)
+  if (!sdsl::cache_file_exists(conf::KEY_GCDA_LSLP, t_config)
+      || !sdsl::cache_file_exists(conf::KEY_GCDA_LSLP_BASIC, t_config)) {
+    auto event = sdsl::memory_monitor::event("DA-LightSLP");
+    ConstructLightSLPOnDA(t_config);
+  }
+}
+
+//~~~~~~~
+
+
+inline void ConstructCombinedSLPOnDA(Config& t_config, uint32_t t_block_size, float t_storing_factor) {
+  auto datafile = cache_file_name(conf::KEY_DA_RAW, t_config);
+
+  grammar::SLP<> slp;
+  {
+    grammar::RePairReader<true> re_pair_reader;
+    auto slp_wrapper = grammar::BuildSLPWrapper(slp);
+    re_pair_reader.Read(datafile, slp_wrapper);
+  }
+
+  auto cslp = grammar::CombinedSLP<>(slp);
+  grammar::Chunks<> cslp_docs;
+
+  grammar::AddSet add_set(cslp_docs);
+  cslp.Compute(t_block_size,
+               add_set,
+               add_set,
+               grammar::MustBeSampled<decltype(cslp_docs)>(grammar::AreChildrenTooBig(cslp_docs, t_storing_factor)));
+
+  sdsl::store_to_cache(cslp, conf::KEY_GCDA_CSLP, t_config);
+
+  auto bit_compress = [](sdsl::int_vector<>& _v) {
+    sdsl::util::bit_compress(_v);
+  };
+
+  sdsl::store_to_cache(cslp_docs, conf::KEY_GCDA_CSLP_DOCS, t_config);
+  grammar::Chunks<sdsl::int_vector<>, sdsl::int_vector<>> cslp_docs_c(cslp_docs, bit_compress, bit_compress);
+  sdsl::store_to_cache(cslp_docs_c, conf::KEY_GCDA_CSLP_DOCS_C, t_config);
+}
+
+void ConstructLightSLPOnDA(Config& t_config) {
+  grammar::LightSLP<> lslp;
+
+  {
+    // Construct Light SLP on DA
+    auto datafile = cache_file_name(conf::KEY_DA_RAW, t_config);
+
+    grammar::SLP<> slp;
+    std::vector<std::size_t> compact_seq;
+    {
+      grammar::RePairReader<false> re_pair_reader;
+      auto slp_wrapper = grammar::BuildSLPWrapper(slp);
+
+      auto report_compact_seq = [&compact_seq](const auto& _var) {
+        compact_seq.emplace_back(_var);
+      };
+
+      re_pair_reader.Read(datafile, slp_wrapper, report_compact_seq);
+    }
+
+    grammar::CombinedSLP<> cslp;
+    sdsl::load_from_cache(cslp, conf::KEY_GCDA_CSLP, t_config);
+
+    lslp.Compute(slp, compact_seq, cslp);
+  }
+
+  sdsl::store_to_cache(lslp, conf::KEY_GCDA_LSLP, t_config);
+
+  // Construct Light SLP Basic on DA
+  auto bit_compress = [](sdsl::int_vector<>& _v) {
+    sdsl::util::bit_compress(_v);
+  };
+  grammar::LightSLP<grammar::BasicSLP<sdsl::int_vector<>>,
+                    grammar::SampledSLP<>,
+                    grammar::Chunks<sdsl::int_vector<>, sdsl::int_vector<>>>
+      lslp_basic(lslp, bit_compress, bit_compress, bit_compress, bit_compress);
+
+  sdsl::store_to_cache(lslp_basic, conf::KEY_GCDA_LSLP_BASIC, t_config);
+}
+
+//~~~~~~~
+
+
+}  // namespace dret
