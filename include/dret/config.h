1ddfd779eb56f118bd5cbcd55abd209edaa3abb2
Dustin Cobas <dustin.cobas@gmail.com>
2026-04-18 18:25:51 -0400

    Refactor `createDefaultKeys` to use `Keys` struct for improved configuration handling
    - Replace `createDefaultKeys` function with `Keys` struct for better encapsulation and reuse.
    - Introduce `kDefaultKeys` as a predefined instance for default configuration keys.
    - Simplify `Config` constructor by using `kDefaultKeys.keys`.

--- include/dret/config.h
+++ include/dret/config.h
@@ -27,23 +27,32 @@ constexpr std::string_view kSLP = "slp";
 constexpr std::string_view kDocs = "docs";
 }  // namespace conf
 
+template <uint8_t t_width = DRET_DEFAULT_ALPHABET_WIDTH>
+struct Keys {
+  Keys() {
+    keys = sri::createDefaultKeys<t_width>();
+
+    keys.update({
+        {conf::kDocEnds, "doc_end"},
+        {conf::kDA, "da"},
+        {
+            conf::kGCDA,
+            {
+                {conf::kSLP, "gcda_slp"},
+                {conf::kDocs, "gcda_docs"},
+            },
+        },
+    });
+  }
+
+  nlohmann::json keys;
+};
+
+const Keys<> kDefaultKeys;
+
 template <uint8_t t_width>
 auto createDefaultKeys() {
-  auto keys = sri::createDefaultKeys<t_width>();
-
-  keys.update({
-      {conf::kDocEnds, "doc_end"},
-      {conf::kDA, "da"},
-      {
-          conf::kGCDA,
-          {
-              {conf::kSLP, "gcda_slp"},
-              {conf::kDocs, "gcda_docs"},
-          },
-      },
-  });
-
-  return keys;
+  return Keys<t_width>().keys;
 }
 
 struct Config : public sri::Config {
@@ -57,7 +66,7 @@ struct Config : public sri::Config {
          bool t_delete_files = false,
          uint8_t t_data_width = 8,
          uint64_t t_data_delim = 0,
-         JSON t_keys = createDefaultKeys<DRET_DEFAULT_ALPHABET_WIDTH>())
+         JSON t_keys = kDefaultKeys.keys)
       : sri::Config(t_data_path, t_output_dir, t_sa_algo, t_delete_files, t_data_width, std::move(t_keys)),
         data_delim(t_data_delim) {}
 };
