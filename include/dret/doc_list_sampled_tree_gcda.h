4f171069ce7eab1fdaaa5db9ae8eef4ea2231f69
Dustin Cobas <dustin.cobas@gmail.com>
2026-04-20 19:19:20 -0400

    Refactor `DocListSampledTreeGCDA` to improve modularity by introducing custom set union functionality and optimizing merge logic

--- include/dret/doc_list_sampled_tree_gcda.h
+++ include/dret/doc_list_sampled_tree_gcda.h
@@ -486,7 +486,74 @@ class MergeSetsBinaryTreeFunctor {
 
   template <typename TII, typename TSets, typename TResult>
   void operator()(TII _first, TII _last, const TSets& _sets, TResult& _result) const {
-    grammar::MergeSetsBinaryTree(_first, _last, _sets, _result);
+    // Refactor function
+    // grammar::MergeSetsBinaryTree(_first, _last, _sets, _result);
+    // using _sets(i) instead _sets[i].
+
+    auto default_set_union = [](auto _first1, auto _last1, auto _first2, auto _last2, auto _result) -> auto {
+      return std::set_union(_first1, _last1, _first2, _last2, _result);
+    };
+
+    (*this)(_first, _last, _sets, _result, default_set_union);
+  }
+
+  template <typename _II, typename _Sets, typename _Result, typename _SetUnion>
+  void operator()(_II _first, _II _last, const _Sets& _sets, _Result& _result, const _SetUnion& _set_union) const {
+    // Refactor function
+    // grammar::MergeSetsBinaryTree(_first, _last, _sets, _result, _set_union);
+    // using _sets(i) instead _sets[i].
+
+    _Result tmp_merge;
+
+    auto merge_tmp = [&tmp_merge, &_set_union](const auto& set1, const auto& set2) {
+      tmp_merge.resize(set1.size() + set2.size());
+      auto last_it = _set_union(set1.begin(), set1.end(), set2.begin(), set2.end(), tmp_merge.begin());
+      tmp_merge.resize(last_it - tmp_merge.begin());
+    };
+
+    auto length = std::distance(_first, _last);
+    if (length == 1) {
+      // merge_tmp(_result, _sets[*_first]);
+      merge_tmp(_result, _sets(*_first));
+
+      _result.swap(tmp_merge);
+
+      return;
+    }
+
+    std::vector<std::pair<uint8_t, _Result>> part_results = {{1, {}}};
+    part_results.front().second.swap(_result);
+
+    while (part_results.size() != 1 || _first != _last) {
+      std::size_t size;
+      while ((size = part_results.size()) > 1
+             && (part_results[size - 1].first == part_results[size - 2].first || _first == _last)) {
+        merge_tmp(part_results[size - 1].second, part_results[size - 2].second);
+
+        part_results[size - 2].second.swap(tmp_merge);
+        ++part_results[size - 2].first;
+        part_results.pop_back();
+      }
+
+      if (_first != _last) {
+        auto next = _first + 1;
+        if (next == _last) {
+          // merge_tmp(part_results.back().second, _sets[*_first]);
+          merge_tmp(part_results.back().second, _sets(*_first));
+
+          part_results.back().second.swap(tmp_merge);
+          ++_first;
+        } else {
+          // merge_tmp(_sets[*_first], _sets[*next]);
+          merge_tmp(_sets(*_first), _sets(*next));
+
+          part_results.emplace_back(1, std::move(tmp_merge));
+          _first += 2;
+        }
+      }
+    }
+
+    _result.swap(part_results.front().second);
   }
 };
 
