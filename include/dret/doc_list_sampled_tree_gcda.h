21ae00482553ca5e0cb85470a01ebcedd777050d
Dustin Cobas <dustin.cobas@gmail.com>
2026-04-20 20:09:08 -0400

    Refactor `DocListSampledTreeGCDA` to remove `SLPWrapper`, streamline `TSLP` usage, and optimize `construct` logic

--- include/dret/doc_list_sampled_tree_gcda.h
+++ include/dret/doc_list_sampled_tree_gcda.h
@@ -15,13 +15,6 @@ namespace dret {
 
 namespace gcda {
 
-template <typename TStorage = GenericStorage,
-          typename TAlphabet = Alphabet<>,
-          typename TSLP = grammar::LightSLP<grammar::BasicSLP<sdsl::int_vector<>>,
-                                            grammar::SampledSLP<>,
-                                            grammar::Chunks<sdsl::int_vector<>, sdsl::int_vector<>>>>
-class SLPWrapper;
-
 using FComputeCover =
     std::function<std::pair<std::pair<std::size_t, std::size_t>, std::vector<std::size_t>>(std::size_t, std::size_t)>;
 
@@ -35,7 +28,9 @@ class MergeSetsBinaryTreeFunctor;
 template <typename TStorage = GenericStorage,
           typename TAlphabet = Alphabet<>,
           typename TCountIdx = sri::SrIdxGeneric<sri::SrIndexValidArea<TStorage, TAlphabet>, 16>,
-          typename TSLP = SLPWrapper<TStorage, TAlphabet>,
+          typename TSLP = grammar::LightSLP<grammar::BasicSLP<sdsl::int_vector<>>,
+                                            grammar::SampledSLP<>,
+                                            grammar::Chunks<sdsl::int_vector<>, sdsl::int_vector<>>>,
           typename TSLPSets = grammar::GCChunks<grammar::BasicSLP<sdsl::int_vector<>>,
                                                 true,
                                                 grammar::Chunks<sdsl::int_vector<>, sdsl::int_vector<>>>,
@@ -57,31 +52,30 @@ class DocListIdxGCDA : public DLSampledTreeScheme<TStorage,
             t_storage,
             TCountIdx(t_storage),
             [this](std::size_t t_bp, std::size_t t_ep) {
-              return this->slp_.ComputeCover(t_bp, t_ep);
+              std::vector<std::size_t> nodes;
+              auto report = [&nodes](const auto& _value) {
+                nodes.emplace_back(_value);
+              };
+              auto range = grammar::ComputeCoverFromBottom(*this->slp_, t_bp, t_ep, report);
+              return std::make_pair(std::move(range), std::move(nodes));
             },
             [this](std::size_t t_bp, std::size_t t_ep, const std::function<void(std::size_t)>& t_report) {
-              this->slp_.ComputeDocs(t_bp, t_ep, t_report);
+              ExpandSLP(*this->slp_, t_bp, t_ep, t_report);
             },
             [this](std::size_t t_i) {
               return (*this->slp_sets_)[t_i];
             },
-            TMergeSets()),
-        slp_(t_storage) {}
+            TMergeSets()) {}
 
   DocListIdxGCDA() = default;
 
   size_type serialize(std::ostream& out, sdsl::structure_tree_node* v, const std::string& name) const override {
     auto child = sdsl::structure_tree::add_child(v, name, sdsl::util::class_name(*this));
-    return this->count_idx_.serialize(out, child, "count_idx") + slp_.serialize(out, child, "slp")
+    return this->count_idx_.serialize(out, child, "count_idx") + slp_->serialize(out, child, "slp")
            + slp_sets_->serialize(out, child, "slp_docs");
   }
 
   const TCountIdx& count_idx = this->count_idx_;
-  const TSLP& slp = slp_;
-
-  const TSLPSets* slp_sets() {
-    return slp_sets_;
-  }
 
   const uint32_t& block_size() const {
     return block_size_;
@@ -98,18 +92,16 @@ class DocListIdxGCDA : public DLSampledTreeScheme<TStorage,
     std::visit(
         [this](auto&& tt_source) {
           this->count_idx_.load(tt_source.get());
-          slp_.load(tt_source.get());
         },
         t_source);
 
     auto key_prefix = std::format("{}-{}_", block_size_, storing_factor_);
-    auto key_docs = key_prefix + t_keys[kGCDA][kDocs].get<std::string>();
-
-    slp_sets_ = this->template loadItemPtr<TSLPSets>(key_docs, t_source, true);
+    slp_ = this->template loadItemPtr<TSLP>(key_prefix + t_keys[kGCDA][kSLP].get<std::string>(), t_source, true);
+    slp_sets_ =
+        this->template loadItemPtr<TSLPSets>(key_prefix + t_keys[kGCDA][kDocs].get<std::string>(), t_source, true);
   }
 
-  TSLP slp_;
-
+  const TSLP* slp_ = nullptr;
   const TSLPSets* slp_sets_ = nullptr;
 
   uint32_t block_size_ = 512;
@@ -131,108 +123,6 @@ void construct(grammar::GCChunks<TSLP, kExpand, grammar::Chunks<sdsl::int_vector
                uint32_t t_block_size,
                float t_storing_factor);
 
-template <typename TStorage,
-          typename TAlphabet,
-          typename TCountIdx,
-          typename TSLP,
-          typename TSLPSets,
-          typename TMergeSets>
-void construct(DocListIdxGCDA<TStorage, TAlphabet, TCountIdx, TSLP, TSLPSets, TMergeSets>& t_index, Config& t_config) {
-  using namespace conf;
-
-  if (!cache_file_exists(t_config.keys[conf::kText].get<std::string>(), t_config)) {
-    auto event = sdsl::memory_monitor::event("Text");
-    ConstructText<TAlphabet::int_width>(t_config);
-  }
-
-  auto count_idx = t_index.count_idx;
-  construct(count_idx, t_config.data_path, t_config);
-
-  auto slp = t_index.slp;
-  construct(slp, t_config);
-
-  if (const auto key = std::format("{}-{}_", t_index.block_size(), t_index.storing_factor())
-                       + t_config.keys[kGCDA][kDocs].get<std::string>();
-      !sdsl::cache_file_exists<TSLPSets>(key, t_config)) {
-    auto event = sdsl::memory_monitor::event(key);
-    TSLPSets slp_sets = t_index.slp_sets() ? *t_index.slp_sets() : TSLPSets();
-    construct(slp_sets, t_config, t_index.block_size(), t_index.storing_factor());
-  }
-
-  t_index.load(t_config);
-}
-
-//~~~~~~~
-
-
-template <typename TStorage, typename TAlphabet, typename TSLP>
-class SLPWrapper : public IndexBaseWithExternalStorage<TStorage, TAlphabet::int_width> {
- public:
-  using Base = IndexBaseWithExternalStorage<TStorage, TAlphabet::int_width>;
-  using typename Base::size_type;
-
-  SLPWrapper(const TStorage& t_storage, uint32_t t_block_size = 512, float t_storing_factor = 4)
-      : Base(t_storage), block_size_(t_block_size), storing_factor_(t_storing_factor) {}
-
-  SLPWrapper(const TSLP* t_slp, uint32_t t_block_size = 512, float t_storing_factor = 4)
-      : slp_(t_slp), block_size_(t_block_size), storing_factor_(t_storing_factor) {}
-
-  SLPWrapper(uint32_t t_block_size, float t_storing_factor)
-      : block_size_(t_block_size), storing_factor_(t_storing_factor) {}
-
-  SLPWrapper() = default;
-
-  auto ComputeCover(std::size_t t_bp, std::size_t t_ep) const {
-    std::vector<std::size_t> nodes;
-
-    auto report = [&nodes](const auto& _value) {
-      nodes.emplace_back(_value);
-    };
-
-    auto range = grammar::ComputeCoverFromBottom(*slp_, t_bp, t_ep, report);
-
-    return std::make_pair(std::move(range), std::move(nodes));
-  }
-
-  template <typename Report>
-  void ComputeDocs(std::size_t t_bp, std::size_t t_ep, Report& t_report) const {
-    ExpandSLP(*slp_, t_bp, t_ep, t_report);
-  }
-
-  const TSLP* slp() const {
-    return slp_;
-  }
-
-  const uint32_t& block_size() const {
-    return block_size_;
-  }
-
-  const float& storing_factor() const {
-    return storing_factor_;
-  }
-
-  size_type serialize(std::ostream& out, sdsl::structure_tree_node* v, const std::string& name) const override {
-    auto child = sdsl::structure_tree::add_child(v, name, sdsl::util::class_name(*this));
-    return slp_->serialize(out, child, "slp");
-  }
-
- protected:
-  void loadInner(typename Base::TSource& t_source, const JSON& t_keys) override {
-    auto key_prefix = std::format("{}-{}_", block_size_, storing_factor_);
-    auto key = key_prefix + t_keys[conf::kGCDA][conf::kSLP].get<std::string>();
-
-    slp_ = this->template loadItemPtr<TSLP>(key, t_source, true);
-  }
-
-  const TSLP* slp_ = nullptr;
-
-  uint32_t block_size_ = 512;
-  float storing_factor_ = 4;
-};
-
-//~~~~~~~
-
-
 template <typename TSLP, typename TSampledSLP, typename TLeavesContainer>
 void construct(grammar::CombinedSLP<TSLP, TSampledSLP, TLeavesContainer>& t_cslp,
                Config& t_config,
@@ -247,12 +137,20 @@ void construct(grammar::LightSLP<TSLP, TSampledSLP, TChunks>& t_lslp,
                uint32_t t_block_size,
                float t_storing_factor);
 
-template <typename TStorage, typename TAlphabet, typename TSLP>
-void construct(SLPWrapper<TStorage, TAlphabet, TSLP>& t_slp, Config& t_config) {
+//~~~~~~~
+
+
+template <typename TStorage,
+          typename TAlphabet,
+          typename TCountIdx,
+          typename TSLP,
+          typename TSLPSets,
+          typename TMergeSets>
+void construct(DocListIdxGCDA<TStorage, TAlphabet, TCountIdx, TSLP, TSLPSets, TMergeSets>& t_index, Config& t_config) {
   using namespace conf;
 
-  if (const auto key = t_config.keys[kText].get<std::string>(); !cache_file_exists(key, t_config)) {
-    auto event = sdsl::memory_monitor::event(key);
+  if (!cache_file_exists(t_config.keys[kText].get<std::string>(), t_config)) {
+    auto event = sdsl::memory_monitor::event("Text");
     ConstructText<TAlphabet::int_width>(t_config);
   }
 
@@ -271,17 +169,27 @@ void construct(SLPWrapper<TStorage, TAlphabet, TSLP>& t_slp, Config& t_config) {
     ConstructDocArray(t_config);
   }
 
-  if (const auto key = std::format("{}-{}_", t_slp.block_size(), t_slp.storing_factor())
-                       + t_config.keys[kGCDA][kSLP].get<std::string>();
+  const auto key_prefix = std::format("{}-{}_", t_index.block_size(), t_index.storing_factor());
+
+  if (const auto key = key_prefix + t_config.keys[kGCDA][kSLP].get<std::string>();
       !sdsl::cache_file_exists<TSLP>(key, t_config)) {
     auto event = sdsl::memory_monitor::event(key);
-    auto key_da = t_config.keys[conf::kDA].get<std::string>();
-    auto filepath_da = sdsl::cache_file_name<std::vector<int>>(key_da, t_config);
-    TSLP slp = t_slp.slp() ? *t_slp.slp() : TSLP();
-    construct(slp, t_config, filepath_da, t_slp.block_size(), t_slp.storing_factor());
+    auto filepath_da = sdsl::cache_file_name<std::vector<int>>(t_config.keys[kDA].get<std::string>(), t_config);
+    TSLP slp;
+    construct(slp, t_config, filepath_da, t_index.block_size(), t_index.storing_factor());
   }
 
-  t_slp.load(t_config);
+  auto count_idx = t_index.count_idx;
+  construct(count_idx, t_config.data_path, t_config);
+
+  if (const auto key = key_prefix + t_config.keys[kGCDA][kDocs].get<std::string>();
+      !sdsl::cache_file_exists<TSLPSets>(key, t_config)) {
+    auto event = sdsl::memory_monitor::event(key);
+    TSLPSets slp_sets;
+    construct(slp_sets, t_config, t_index.block_size(), t_index.storing_factor());
+  }
+
+  t_index.load(t_config);
 }
 
 //~~~~~~~
